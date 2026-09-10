d9577ccab4fa0abbe1d6a8ab89ad5e942fb94255
MrHakan <hakan.efe.gulacti@gmail.com>
2026-09-10 12:04:32 +0100

    Add configurable Right Shift ClickGUI key mapping

--- src/main/java/me/mrhakan/agalarhack/AgalarHackClient.java
+++ src/main/java/me/mrhakan/agalarhack/AgalarHackClient.java
@@ -1,16 +1,22 @@
 package me.mrhakan.agalarhack;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import me.mrhakan.agalarhack.managers.CommandManager;
 import me.mrhakan.agalarhack.managers.FriendManager;
 import me.mrhakan.agalarhack.managers.KeybindManager;
 import me.mrhakan.agalarhack.managers.ModuleManager;
 import me.mrhakan.agalarhack.managers.SettingsManager;
+import me.mrhakan.agalarhack.ui.ClickGuiScreen;
 import me.mrhakan.agalarhack.ui.Hud;
+import me.mrhakan.agalarhack.ui.ModuleSettingsScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
 import net.fabricmc.fabric.api.client.message.v1.ClientSendMessageEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.resources.Identifier;
+import org.lwjgl.glfw.GLFW;
 
 public class AgalarHackClient implements ClientModInitializer {
 
@@ -23,17 +29,39 @@ public class AgalarHackClient implements ClientModInitializer {
 	public static final SettingsManager SETTINGS_MANAGER = new SettingsManager();
 	public static final FriendManager FRIEND_MANAGER = new FriendManager();
 
+	private static final KeyMapping.Category KEY_CATEGORY = KeyMapping.Category.register(
+			Identifier.fromNamespaceAndPath(MOD_ID, "client"));
+	private static KeyMapping clickGuiKey;
+
 	@Override
 	public void onInitializeClient() {
 		FRIEND_MANAGER.load();
 		moduleManager.loadModules();
 		CommandManager.init();
 
+		clickGuiKey = KeyMappingHelper.registerKeyMapping(new KeyMapping(
+				"key.agalarhack.open_gui",
+				InputConstants.Type.KEYSYM,
+				GLFW.GLFW_KEY_RIGHT_SHIFT,
+				KEY_CATEGORY));
+
 		ClientSendMessageEvents.ALLOW_CHAT.register(message -> !CommandManager.handleChat(message));
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			KeybindManager.tick(client);
 			moduleManager.tick(client);
+
+			while (clickGuiKey.consumeClick()) {
+				if (client.player == null) {
+					continue;
+				}
+				if (client.gui.screen() instanceof ClickGuiScreen
+						|| client.gui.screen() instanceof ModuleSettingsScreen) {
+					client.gui.setScreen(null);
+				} else {
+					client.gui.setScreen(new ClickGuiScreen());
+				}
+			}
 		});
 
 		HudElementRegistry.addLast(Identifier.fromNamespaceAndPath(MOD_ID, "hud"), new Hud());
